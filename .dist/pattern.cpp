133608b060377063ce66b7d2b99716cba05212e0
priyank parikh <priyankparikh.17@gmail.com>
2026-08-31 17:23:19 +0530

    first commit

---
+++ .dist/pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void print1(){
+    for(int i = 0; i<= 4 ; i++){
+        for(int j = 0; j<= i ; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+
+
+int main(){
+  
+    print1();
+    return 0;
+}
