f77708a56757e9a95191e6b62aa4bedf91d217e4
priyank parikh <priyankparikh.17@gmail.com>
2026-09-22 17:17:32 +0530

    find all occurence of a string

---
+++ .dist/removealloccurence.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string s = "daabcbaabcbc";
+    string part = "abc";
+
+    while (s.length() > 0 && s.find(part) < s.length()) {
+        s.erase(s.find(part), part.length());
+    }
+
+    cout << s << endl;
+
+    return 0;
+}
